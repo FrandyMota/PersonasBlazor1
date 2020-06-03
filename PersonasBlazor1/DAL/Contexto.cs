a1578c60a4971eb27a944652491b3b17dd0eebc3
Frandy Francisco Mota Rodriguez <frandy_mota@ucne.edu.do>
2020-06-02 20:57:07 -0700

    Add project files.

---
+++ PersonasBlazor1/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonasBlazor1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonasBlazor1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Data\Personas.db");
+        }
+
+
+    }
+}
